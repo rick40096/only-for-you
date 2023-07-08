55751e7cac4290f7853e615810f1f84c51937745
ℕ@._.օռɛ <84985321+rick40096@users.noreply.github.com>
2023-07-09 00:28:35 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+X = "cucumber"
+Y = "banana"
+Z = "watermelon"
+A = "pinnaple" 
+B = "jackfruit" 
+
+x = input("choose one number b/w 1 to 5 --")
+h = int(x)
+
+if h == 1 : print("put " + X +" in ur ass")
+   
+elif h == 2 : print("put " + Y +" in ur ass")
+
+elif h == 3 : print("put "+ Z +" in ur ass")
+
+elif h == 4 : print("put " + A +" in ur ass")
+
+elif h == 5 : print("put " + B +" in ur ass")
+
+else :
+  print("fuck off")
