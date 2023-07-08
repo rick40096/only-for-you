6c6db36d3c5ae95ccbe37774a8cba95a47563496
ℕ@._.օռɛ <84985321+rick40096@users.noreply.github.com>
2023-07-09 01:27:25 +0530

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,62 @@
+print("Welcome to fun land....")
+
+print("\nPlease!!do as directed")
+
+print("\nif you r offensive please leave ....")
+
 X = "cucumber"
 Y = "banana"
 Z = "watermelon"
 A = "pinnaple" 
 B = "jackfruit" 
 
-x = input("choose one number b/w 1 to 5 --")
+x = input("\nchoose one number b/w 1 to 5 --")
 h = int(x)
 
-if h == 1 : print("put " + X +" in ur ass")
+if h == 1 : print("\nput " + X +" in ur ass")
    
-elif h == 2 : print("put " + Y +" in ur ass")
+elif h == 2 : print("\nput " + Y +" in ur ass")   
+  
+elif h == 3 : print("\nput "+ Z +" in ur ass")
 
-elif h == 3 : print("put "+ Z +" in ur ass")
+elif h == 4 : print("\nput " + A +" in ur ass")
 
-elif h == 4 : print("put " + A +" in ur ass")
-
-elif h == 5 : print("put " + B +" in ur ass")
+elif h == 5 : print("\nput " + B +" in ur ass")
 
 else :
-  print("fuck off")
+  print("\nfuck off")
+
+print("\nmy bad, sorry for that, something went wrong......")
+
+print("\nplease do it once more")
+
+
+
+R = "asshole"
+T = "dumbass"
+S = "dick head"
+W = "butt faced"
+Q = "whore"
+
+p = input("\nchoose a number between 6 to 10...")
+q = int(p)
+
+if q == 6 : print("\nyou are a " + R )
+   
+elif q == 7 : print("\nyou are a " + T )   
+  
+elif q == 8 : print("\nyou are a "+ S )
+
+elif q == 9 : print("\nyou are a " + W )
+
+elif q == 10 : print("\nyou are a " + Q )
+
+else :
+  print("\nmotherfucker don't you know counting")
+
+
+print("\nThank You !!!")
+
+
+
+
